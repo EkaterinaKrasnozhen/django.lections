c0fcd7c6e45afccd8a8f87a316c8ef8513497fa2
kate <01redcat@mail.ru>
2023-09-04 11:33:32 +0300

    представления view'

---
+++ Proj/myproject/myapp3/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import hello, HelloView
+from .views import year_post, MonthPost, post_detail, my_view, TemplIf, view_for, index, about
+
+urlpatterns = [
+    path('hello/', hello, name='hello'),
+    path('hello2/', HelloView.as_view(), name='hello2'), # предсавление на основе класса
+    path('posts/<int:year>/', year_post, name='year_post'),
+    path('posts/<int:year>/<int:month>/', MonthPost.as_view(), name='month_post'),
+    path('posts/<int:year>/<int:month>/<slug:slug>/', post_detail, name='post_detail'), # slug - набор текста, строка с буквами, цифрами тд, uuid приставка для уник идентиф
+    path('', my_view, name='index'),
+    path('if', TemplIf.as_view(), name='templ_if'),
+    path('for/', view_for, name='templ_for'),
+    path('index/', index, name='index'),
+    path('about/', about, name='about'),
+]
+
